23b4641cefa833800ee0ad6a972a15f81bba2052
Alan Flores perez <alan_perez001@outlook.es>
2020-09-29 14:00:01 -0500

    Encode and Decode

--- Ubox/GenerarCodigo.xaml.cs
+++ Ubox/GenerarCodigo.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,6 +22,7 @@ namespace Ubox
     /// </summary>
     public partial class GenerarCodigo : Page
     {
+        static string key { get; set; } = "A!9HHhi%XjjYY4YP2@Nob009X";
         public GenerarCodigo()
         {
             InitializeComponent();
@@ -39,6 +41,59 @@ namespace Ubox
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void PassMd5(object sender, RoutedEventArgs e)
+        {
+            var text = "Esta es una cadena de prueba";
+            Console.WriteLine("Cadena: "+text);
+
+            var cipher = Encrypt(text);
+            Console.WriteLine("Codificado: "+cipher);
+
+            text = Decrypt(cipher);
+            Console.WriteLine("Decodificado: "+text);
+
+            Console.Read();
+        }
+        public static string Encrypt(string text)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateEncryptor())
+                    {
+                        byte[] textBytes = UTF8Encoding.UTF8.GetBytes(text);
+                        byte[] bytes = transform.TransformFinalBlock(textBytes, 0, textBytes.Length);
+                        return Convert.ToBase64String(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+        }
+
+        public static string Decrypt(string cipher)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateDecryptor())
+                    {
+                        byte[] cipherBytes = Convert.FromBase64String(cipher);
+                        byte[] bytes = transform.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                        return UTF8Encoding.UTF8.GetString(bytes);
+                    }
+                }
+            }
         }
     }
 }
