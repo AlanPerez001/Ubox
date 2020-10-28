e34f9045f62771859efe5b9491fac94c03186cf4
Alan Flores perez <alan_perez001@outlook.es>
2020-10-28 09:43:56 -0600

    Logo central ubox

--- Ubox/obj/Debug/Ubox_Content.g.i.cs
+++ Ubox/obj/Debug/Ubox_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenes/checksinmarcarrelleno.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenes/logocentral.png")]
 
 
